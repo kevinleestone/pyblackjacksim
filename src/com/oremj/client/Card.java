403e3d400aa11ec2611f537347579cf287ae1e71
jeremy.orem <jeremy.orem@d97df1da-9649-0410-bed5-23986428cd2b>
2008-04-10 20:58:05 +0000

    game "works"

--- src/com/oremj/client/Card.java
+++ src/com/oremj/client/Card.java
@@ -5,16 +5,37 @@ import com.google.gwt.user.client.ui.Image;
 public class Card {
 	private String suit;
 	private String ident;
+	private boolean facedown;
 	public Card(String suit, String ident) {
 		this.suit = suit;
 		this.ident = ident;
+		facedown = false;
 	}
 		
 	public String toURL() {
 		return "img/" + suit + "-" + ident + "-150.png";
 	}
-	
+	public void flip() {
+		facedown = ! facedown;
+	}
 	public Image getImage() {
+		if (facedown)
+			return new Image("img/back-blue-150-3.png");
 		return new Image(toURL());
+	}
+	
+	public boolean isFacedown() {
+		return facedown;
+	}
+	
+	public int value() {
+		if ( ident == "j" ||
+				ident == "q" ||
+				ident == "k" ) {
+			return 10;
+		} else if (ident == "a") {
+			return 11;
+		} else 
+			return Integer.parseInt(ident);
 	}
 }
--- src/com/oremj/client/Player.java
+++ src/com/oremj/client/Player.java
@@ -8,23 +8,59 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class Player {
 	private String name;
 	private Hand hand;
+	private float bankroll;
 	final private HorizontalPanel hand_panel;
+	final private Label bankroll_label;
 	public Player(String name) {
+		bankroll = 1000;
+		bankroll_label = new Label();
 		this.name = name;
 		this.hand = new Hand();
 		hand_panel = new HorizontalPanel();
-		RootPanel.get().add(new Label(name));
-		RootPanel.get().add(hand_panel);
+		refreshBankroll();
+
 	}
 	
+	private void refreshBankroll() {
+		bankroll_label.setText(String.valueOf(bankroll));
+	}
 	public void endHand() {
+		hand.allFaceUp();
+		displayHand();
 		hand.destroyHand();
 	}
-	public void getCard(Card card) {
+
+	public Card getCard(Card card) {
 		hand.addCard(card);
+		return card;
 	}
 	
 	public void displayHand() {
 		hand.displayHand(hand_panel);
+	}
+	
+	public int handValue() {
+		return hand.handValue();
+	}
+	
+	public Label getNameLabel() {
+		return new Label(name);
+	}
+	
+	public Label getBankrollLabel() {
+		return bankroll_label;
+	}
+	public HorizontalPanel getPanel() {
+		return hand_panel;
+	}
+	public int takeMoney(int amount) {
+		bankroll -= amount;
+		refreshBankroll();
+		return amount;
+	}
+	
+	public void giveMoney(float amount) {
+		bankroll += amount;
+		refreshBankroll();
 	}
 }
